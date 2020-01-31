c6a1685248fb9a62fc5aba7863f5b562f78d1017
blacchoy@gmail.com <blacchoy@gmail.com>
2020-01-31 12:59:35 -0800

    add 1st constuctor to js file, starting to write function to calculate the cost of pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 // business logic below
-function Pizza (pizzaTopping, pizzaSize) {
-  this.pizzaTopping = pizzaTopping,
-  this.pizzaSize = pizzaSize,
-  this.cost = 0
+function Pizza (pizzaSize) {
+   this.pizzaSize = pizzaSize,
+   this.cost = 0
 };
+
+function pizzaPrice(pizza) {
+  if (pizza.pizzaSize === "personal") {
+    pizza.cost = 7.50
+    console.log(pizza.cost)
+  }
+}
 
 // user interface logic below
 
 $(document).ready(function() {
-  $("#input").ready(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
-
-    $("#output").text(result);
+    var size = $("#size").val();
+    var pizza = new Pizza(size);
+    pizzaPrice(pizza);
+    // console.log(size)
   });
 });
+
